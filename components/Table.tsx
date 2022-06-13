342529d4b5a1d722fb56d55a3c653b22cceaef04
Michael White <michaelwhite404@gmail.com>
2022-06-12 22:58:40 -0400

    Dark mode for custom table

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,7 +1,14 @@
+import { useMantineTheme } from "@mantine/core";
+
 export default function Table(props: TableProps) {
+  const theme = useMantineTheme();
   const { headers, data } = props;
   return (
-    <table className="custom-table" cellSpacing="0" cellPadding="0">
+    <table
+      className={`custom-table ${theme.colorScheme === "dark" ? "dark" : ""}`}
+      cellSpacing="0"
+      cellPadding="0"
+    >
       <thead>
         <tr>
           {headers.map((header) => (
