f8684752e2b7c46f27457779bc9633361da76c8f
Michael White <michaelwhite404@gmail.com>
2022-06-17 02:42:11 -0400

    Language selector dark mode

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,18 +1,25 @@
+import { useMantineTheme } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
+import classnames from "classnames";
 import React from "react";
 
 const languages = ["shell", "javascript"];
 
 export default function LanguageSelector() {
+  const theme = useMantineTheme();
   const [language, setLanguage] = useLocalStorage({
     key: "language",
     defaultValue: "shell",
     getInitialValueInEffect: true,
   });
   return (
-    <div className="language-selector">
+    <div className={classnames("language-selector", { dark: theme.colorScheme === "dark" })}>
       {languages.map((l) => (
-        <div key={l} className={language === l ? "active" : ""} onClick={() => setLanguage(l)}>
+        <div
+          key={l}
+          className={classnames({ active: language === l })}
+          onClick={() => setLanguage(l)}
+        >
           {l}
         </div>
       ))}
